64c78cd496f822128f32ad404114b788aabd5278
RIchardIDE <puriboonrang@gmail.com>
2026-06-22 18:18:58 +0700

    Fix Vercel 404 & Add Multi-page config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        explorer: 'homepage/explorer.html',
+        workspace: 'homepage/workspace.html',
+        register: 'homepage/register.html',
+        // 💡 ถ้ากัปตันมีไฟล์ HTML อยู่ในโฟลเดอร์อื่น ให้ชี้พาธแบบนี้ครับ
+        // homepage: 'homepage/homepage.html',
+        // admin: 'admin/admin.html'
+      }
+    }
+  }
 });
